4a5838ea5494d421ef29204d06227cd06be698be
Piotr Walat <pwalat98@gmail.com>
2020-12-12 20:48:11 +0100

    add ValueHandler and DontDestroyOnLoad

---
+++ Assets/Scripts/ValueHolderScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ValueHolderScript : MonoBehaviour
+{
+    public int minutes;
+    public int percentageOfBigCars;
+    public int greenLightSeconds;
+    public int yellowLightSeconds;
+    public int tBlockTimeSeconds;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
