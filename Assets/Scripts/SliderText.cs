7fa43f8bc23a230244319f8f340e10ec1e278a0c
Piotr Walat <pwalat98@gmail.com>
2020-12-12 19:20:06 +0100

    add texts reaction to input

---
+++ Assets/Scripts/SliderText.cs
@@ -0,0 +1,19 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderText : MonoBehaviour
+{
+    public TextMeshProUGUI sliderText;
+    public Slider slider;
+
+    private void Start()
+    {
+        slider = GetComponentInParent<Slider>();
+    }
+
+    void Update()
+    {
+        sliderText.text = slider.value.ToString();
+    }
+}
---
+++ Assets/Scripts/ValidationOfInput.cs
@@ -0,0 +1,56 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class ValidationOfInput : MonoBehaviour
+{
+    public TextMeshProUGUI inputText;
+    public TMP_InputField input;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        input = GetComponentInParent<TMP_InputField>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            if (input.text != "")
+            {
+                if (Validation(input.text))
+                {
+                    AssignTextWithValue();
+                }
+                else
+                {
+                    input.text = "has to be digit only";
+                }
+            }
+        }
+    }
+
+    void AssignTextWithValue()
+    {
+        inputText.text = input.text;
+        input.text = "";
+    }
+
+    bool Validation(string input)
+    {
+        return IsDigitsOnly(input);
+    }
+
+    bool IsDigitsOnly(string str)
+    {
+        foreach (char c in str)
+        {
+            if (c < '0' || c > '9')
+                return false;
+        }
+
+        return true;
+    }
+}
