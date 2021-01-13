040fcd8e41d13e3afeaff98d57a70d4b9f83863a
Piotr Walat <pwalat98@gmail.com>
2021-01-14 00:42:59 +0100

    timer UI and abandon Button

---
+++ Assets/Scripts/AbandonAttempt.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class AbandonAttempt : MonoBehaviour
+{
+    public Button abandonButton;
+    private Animator transition = null;
+    public float transitionTime = 1f;
+
+    private void Awake()
+    {
+        transition = GameObject.FindGameObjectWithTag("animator").GetComponent<Animator>();
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        abandonButton.onClick.AddListener(GoToMainMenu);
+    }
+
+    // Update is called once per frame
+    void GoToMainMenu()
+    {
+        StartCoroutine(LoadLevel(0));
+    }
+
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(transitionTime);
+
+        SceneManager.LoadScene(levelIndex);
+    }
+}
---
+++ Assets/Scripts/TimerUI.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerUI : MonoBehaviour
+{
+    public Text TimerText;
+    public GameObject globalTimerObject;
+    public GlobalTimerScript globalTimer;
+
+    private float Timer;
+
+    private void Start()
+    {
+        globalTimer = globalTimerObject.GetComponent<GlobalTimerScript>();
+        Timer = globalTimer.timer;
+    }
+
+    void Update()
+    {
+        Timer = globalTimer.timer;
+        int minutes = Mathf.FloorToInt(Timer / 60F);
+        int seconds = Mathf.FloorToInt(Timer % 60F);
+        int milliseconds = Mathf.FloorToInt((Timer * 100F) % 100F);
+        TimerText.text = minutes.ToString("00") + ":" + seconds.ToString("00") + ":" + milliseconds.ToString("00");
+        
+    }
+
+}
