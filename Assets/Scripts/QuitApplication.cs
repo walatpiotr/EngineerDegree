c86c266975b4dff6788ee8500e34b633de0197bd
Piotr Walat <pwalat98@gmail.com>
2021-01-14 11:46:44 +0100

    free camera added

---
+++ Assets/Scripts/QuitApplication.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class QuitApplication : MonoBehaviour
+{
+    public void doExitGame()
+    {
+        Debug.Log("Quiting");
+        Application.Quit();
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+}
